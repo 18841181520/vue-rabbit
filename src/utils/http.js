9d898e8fa91986b1e2819d3d59887e9f71034317
yaoyafeng <1051483901@qq.com>
2023-12-19 21:21:37 +0800

    9

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import {ElMessage} from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import {useUserStore} from '@/stores/user.js';
+import router from '@/router'
 
 // 创建axios实例
 const http = axios.create({
@@ -8,8 +10,14 @@ const http = axios.create({
     timeout: 5000
 })
 
+
 // axios请求拦截器
 http.interceptors.request.use(config => {
+    const userStore = useUserStore()
+    const token = userStore.userInfo.token
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
     return config
 }, e => Promise.reject(e))
 
@@ -19,6 +27,11 @@ http.interceptors.response.use(res => res.data, e => {
         type: 'warning',
         message: e.response.data.message
     })
+    const userStore = useUserStore()
+    if (e.response.status === 401) {
+        userStore.clearUserInfo()
+        router.push('/login')
+    }
     return Promise.reject(e)
 })
 
